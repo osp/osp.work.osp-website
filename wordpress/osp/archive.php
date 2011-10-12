6729bf5387defcf7da7771fb37afdbae147de93b
codingisacopingstrategy <eric@ericschrijver.nl>
2011-10-12 23:26:39 +0200

    So lets put it back to the one I had stored previously!
    Now the archives work again

--- wordpress/osp/archive.php
+++ wordpress/osp/archive.php
@@ -1,36 +1,69 @@
-<?php get_header() ?>
+<?php get_header(); ?>
+		
+<div id="content_box">
 
-	<div id="container">
-		<div id="content">
+	<div id="left_side">
+	
+		<?php if (have_posts()) : ?>
 
-<?php the_post() ?>
+			<?php //$post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+	
+			<?php /* If this is a category archive */ if (is_category()) { ?>				
+			<h1><span class="label">Category:</span> <?php echo single_cat_title(); ?></h1>
+			<p class="description"><?php echo category_description(); ?></p>
+			
+			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+			<h1><?php the_time('F Y'); ?></h1>
 
-<?php if ( is_day() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Daily Archives: <span>%s</span>', 'sandbox' ), get_the_time(get_option('date_format')) ) ?></h2>
-<?php elseif ( is_month() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Monthly Archives: <span>%s</span>', 'sandbox' ), get_the_time('F Y') ) ?></h2>
-<?php elseif ( is_year() ) : ?>
-			<h2 class="page-title"><?php printf( __( 'Yearly Archives: <span>%s</span>', 'sandbox' ), get_the_time('Y') ) ?></h2>
-<?php elseif ( isset($_GET['paged']) && !empty($_GET['paged']) ) : ?>
-			<h2 class="page-title"><?php _e( 'Blog Archives', 'sandbox' ) ?></h2>
-<?php endif; ?>
+			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
+			<?php 
+			if(isset($_GET['author_name'])) :
+			$curauth = get_userdatabylogin($author_name); // NOTE: 2.0 bug requires get_userdatabylogin(get_the_author_login());
+			else : 
+			$curauth = get_userdata(intval($author));
+			endif ; ?>
+			<h1><span class="label">Author:</span> <?php wp_title(''); ?></h1>
 
+			<?php /* If this is a tag archive */ } elseif (is_tag()) { ?>
+			<h1><span class="label">Tag:</span> <?php single_tag_title(); ?></h1>
+			<p><?php echo $curauth->description; ?></p>
+		
+			<?php } ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
+		<div class="listing">
 
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __( 'Permalink to %s', 'sandbox' ), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h3>
-				<div class="entry-content">
-<?php the_excerpt( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
+			<?php while (have_posts()) : the_post(); ?>
+			<div class="post">
+				<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
+				<p class="meta"><?php the_time('F jS, Y') ?> &middot; <?php the_tags('Tags: ', ' &middot; ', ''); ?> &middot; <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?><?php edit_post_link('Edit',' &middot; ',''); ?></p>
+				<div class="entry">
+					<!--IF THERE IS A THUMBNAIL ATTACHED, DISPLAY IT-->
+					<?php $thumb = get_post_meta($post->ID, "thumb", true); if ($thumb != "") 
+					{ ?><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><img src="<?php $values = get_post_custom_values("thumb"); echo $values[0]; ?>" alt="<?php the_title(); ?>" class="thumb" /></a><?php } ?>
+					<?php the_excerpt() ?>
+				</div><!--entry-->
+				<div class="clear"></div>
+			</div><!--post-->
+			
+		<?php endwhile; ?>
+			
+		<?php else : ?>
+	
+			<h1>Nothing found</h1>
+			<div class="entry">
+				<p><?php include (TEMPLATEPATH . '/searchform.php'); ?></p>
+			</div>
+		<?php endif; ?>
+	</div><!--listing-->
 
-				</div>
-				<?php include('meta.php'); ?>
-			</div><!-- .post -->
+	</div><!--left_side-->
 
-<?php endwhile; ?>
+		<div class="sidebar">
+			<?php include (TEMPLATEPATH . '/sidebar.php'); ?>
+		</div>
+		<div class="r_sidebar">
+			<?php include (TEMPLATEPATH . '/r_sidebar.php'); ?>
+		</div>
+</div><!--content_box-->
 
-		</div><!-- #content .hfeed -->
-	</div><!-- #container -->
-
-<?php get_sidebar() ?>
-<?php get_footer() ?>
+<?php get_footer(); ?>
