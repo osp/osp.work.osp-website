e3aaf79d2488900fcd2f6737093cee99f19afe17
Eric Schrijver <eric@ericschrijver.nl>
2012-07-02 15:55:06 +0200

    More website changes snapshots

--- layouts/index.php
+++ layouts/index.php
@@ -9,6 +9,7 @@
     <meta name="author" content="vous">
     <link rel="stylesheet" type="text/css" media="all" href="reset.css" />
     <link rel="stylesheet/less" type="text/css" href="styles-start.less">
+    <!-- <link rel="stylesheet/less" type="text/css" href="http://osp.titanpad.com/ep/pad/export/160/latest?format=txt" /> -->
     <script src="less-1.3.0.min.js" type="text/javascript"></script>
 
    
@@ -50,7 +51,12 @@
 	</div>
 -->
 
+
 	<div id="active-projects">
+
+		<div id="home-infos">
+		What is that? Some says that those snapshots are to our work what the pictures of a cookbook are to the cooked meal. source files = ingredients. These arrangements of blocks of text and pictures tries harder to depict the most current projects, some interesting visual extracts and the source files that provide a convenient way to access to study it, change it, to redistribute copies of it, to improve it and release your improvements (and modified versions in general). Nous sommes embarqués.<a href="http://osp.constantvzw.org/about"> Read more about the way we work → </a>
+		</div>
 	
 <!--
         <div class="legende-box"> 
@@ -61,21 +67,30 @@
                  <?php //require "osp-commits.php"; ?>
 	</div>
 
-    <div id="menu">
-        <li><a href="http://osp.schr.fr/work/">Works</a></li>
+    <ul id="menu">
+        <li><a href="http://ospwork.constantvzw.org/work/">Works</a></li>
         <li><a href="http://osp.constantvzw.org/foundry/">Foundry</a></li>
-        <li><a href="http://osp.schr.fr/workshop/">Workshops</a></li>
-        <li><a href="http://osp.schr.fr/tools/">Tools</a></li>
+        <li><a href="http://ospwork.constantvzw.org/workshop/">Workshops</a></li>
+        <li><a href="http://ospwork.constantvzw.org/tools/">Tools</a></li>
         <li><a href="http://osp.constantvzw.org/blog/">Blog</a></li>
         <li><a href="http://osp.constantvzw.org/images/">Images</a></li>
         <li><a href="http://osp.constantvzw.org/about">About</a></li>
-        <li><a href="http://osp.constantvzw.org/contact">Contact</a></li>
 
         <object type="image/svg+xml" style="width:60px;height:60px;" data="./pict/OSP_new-frog.svg">
             <img src="./pict/OSP_new-frog.png" style="width:60px;height:60px;" alt="A stylised image of a frog." />
         </object>
-            OSP  
-    </div>
+OSP, 
+    <?php
+$filename = "osp-baselines.txt";
+$handle = fopen($filename, "r");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+$words = explode("---", $contents);
+echo "<p id='#baseline'>".$words[rand(0, count($words)-1)]."</p>";
+?>
+
+
+    </ul>
 
 
 </body>
--- layouts/osp-iceberg.php
+++ layouts/osp-iceberg.php
@@ -1,5 +1,5 @@
 <?php
-$url='http://osp.schr.fr/repos.json'; 
+$url='http://ospwork.constantvzw.org/repos.json'; 
 $repos = json_decode(get_data($url), true);
 ?>
 
@@ -14,12 +14,16 @@ $repos = json_decode(get_data($url), true);
  }
 
 $iceberg_repos = array_slice($iceberg_repos, 0, 8);
+
+$filename = "said.txt";
+$handle = fopen($filename, "r");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+$words = explode("---", $contents);
  
  foreach($iceberg_repos as $repo) {
      
      if(strtotime($repo["last_updated"]) > strtotime("Last Month")) {
-         //echo "is from last month";
-     }
 
      $ids = explode('.', $repo['slug']);
      $type = $ids[1];
@@ -27,36 +31,49 @@ $iceberg_repos = array_slice($iceberg_repos, 0, 8);
          <div class='project'>
             <p class='project-type'>$type</p>
             <h2 class='project-title'>
-                <a href='http://osp.schr.fr/$repo[web_path]/'>
+                <a href='http://ospwork.constantvzw.org/$repo[web_path]/'>
                     $repo[title]
                 </a>
             </h2>
             <div class='iceberg'>";
             $iceberg_img = array_slice($repo["iceberg"], 0, 4);
-            foreach($iceberg_img as $img) {
-                echo "<img class='iceberg-pict' src='http://osp.schr.fr/$repo[web_path]/thumbnail/latest/iceberg/$img' />";
+            foreach($iceberg_img as $index => $img) {
+                if ($index == 0) {
+                    echo "<a href='http://ospwork.constantvzw.org/$repo[web_path]/'><img class='iceberg-pict-big' src='http://ospwork.constantvzw.org/$repo[web_path]/thumbnail/latest/iceberg/$img' /></a>";
+                } else {
+                    echo "<a href='http://ospwork.constantvzw.org/$repo[web_path]/'><img class='iceberg-pict' src='http://ospwork.constantvzw.org/$repo[web_path]/thumbnail/latest/iceberg/$img' /></a>";
+                }
             }
      echo "
             </div>
-            <p class='project-source'><a href='http://osp.schr.fr/$repo[web_path]/view/latest/'>Source files</a></p>
-            <div class='commit-list'>";
-                $commit_list = array_slice($repo["commits"], 0, 3);
-                foreach($commit_list as $commit) {
+            <p class='project-source'><a href='http://ospwork.constantvzw.org/$repo[web_path]/view/latest/'>Source files</a></p>
 
+            <div class='commit-list'>";
+
+                $commit_list = array_slice($repo["commits"], 0, 5);
+                $ellipse = 0;
+                foreach($commit_list as $index => $commit) {
+                    if ($index != 0) {
+                        $ellipse = (strtotime($previous_commit) - strtotime($commit["date"]))/(24*60*60);
+                        $ellipse = $ellipse *3;
+                    }
                     echo "
+                        <div class='ellipse' style='width: 5px; margin: auto; text-align: center; height: ".$ellipse."px; border-left: 1px solid springgreen;'>&nbsp;</div>
                         <div class='commit'>
-                            <p class='commit-author'>$commit[author] <span class='commit-author-said'>said&thinsp;:</span></p>
-                            <p class='commit-message'>$commit[message]</p>
+                        <p><span class='commit-author'>$commit[author]</span><span class='commit-author-said'>".$words[rand(0, count($words)-1)]."
+                        </span></p>
+                            <p class='commit-message'>&mdash; $commit[message]</p>
                             <p class='commit-date'>".date('l, j F Y', strtotime($commit[date]))."</p>
                         </div>
                     ";
+                    $previous_commit = $commit["date"];
                 }
 
 
             echo "        
             </div>
         </div>";
-     //}
+     }
   }
  ?>
  <?php
