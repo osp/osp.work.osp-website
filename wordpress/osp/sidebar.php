ff7ef65dc5636af116a35b5bbe10900e984bb58f
sarah <sarah@sarah-ThinkPad-T410.(none)>
2014-01-28 18:08:27 +0100

    searching z-index

--- wordpress/osp/sidebar.php
+++ wordpress/osp/sidebar.php
@@ -5,11 +5,11 @@
 		<?php endif; ?>
 	</li>
 
-  <li class="widget">
+  <li class="widget" style="margin-top:0px;margin-left:0px;">
     <?php include (TEMPLATEPATH . '/searchform.php'); ?>
   </li>
 
-  <li class="widget"><h2><?php _e('Admin'); ?></h2>
+  <li class="widget" style="margin-top:0px;margin-left:0px;"><h2><?php _e('Admin'); ?></h2>
     <ul>
       <?php wp_register(); ?>
       <li><?php wp_loginout(); ?></li>
